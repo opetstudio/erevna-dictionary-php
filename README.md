384899162f14b19b583ea5b58939e2c4f6d75b1a
Nofrets <opetstudio@gmail.com>
2017-10-06 13:36:29 +0700

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 API dictionary
+
+###deploy
+
+  deploy di mesin elasticsearch
+
+###Endpoint
+
+  - /index.php?keyfrom=propertyName&text=apt
+    response:
+
+      {status:true, text:"apt", meaning:"apartment"}
--- src/Main.php
+++ src/Main.php
@@ -8,13 +8,13 @@
     public static function main(){
       $self = new Main();
       // get the HTTP method, path and body of the request
-      $method = $_SERVER['REQUEST_METHOD'];
-      $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-      $input = json_decode(file_get_contents('php://input'),true);
+      // $method = $_SERVER['REQUEST_METHOD'];
+      // $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+      // $input = json_decode(file_get_contents('php://input'),true);
       //
       // // retrieve the table and key from the path
-      $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-      $key = array_shift($request)+0;
+      // $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
+      // $key = array_shift($request)+0;
       //
       // // escape the columns and values from the input object
       // $columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
@@ -28,18 +28,27 @@
       //   http_response_code(404);
       //   die(mysqli_error());
       // }
+
+      //input
+      $text = $_GET["text"];
+      $keyfrom = $_GET["keyfrom"];
+
+      //output
+      $status = true;
+      $meaning = $self->getMeaningOfText($text);
+
       $myObj = new \stdClass();
-      $myObj->status = true;
-      $myObj->table = $table;
-      $myObj->verifycode = 452367;
+      $myObj->status = $status;
+      $myObj->text = $text;
+      $myObj->meaning = $meaning;
 
       $myJSON = json_encode($myObj);
-
       echo $myJSON;
-
       // echo $self->createUniqCode();
     }
-    public function createUniqCode(){
+    public function getMeaningOfText($text = ""){
+      //git list data dictionary dari elasticsearch
+      //hit api erevna-es-interface-js
       return "123456789";
     }
   }
